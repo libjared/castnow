6d2a353dac8fbc0a28c115f3f795467f0e5c4ad0
libjared <libjared@users.noreply.github.com>
2016-01-09 16:34:40 -0500

    wip: selective transcoding

--- plugins/transcode.js
+++ plugins/transcode.js
@@ -5,6 +5,8 @@ var Transcoder = require('stream-transcoder');
 var grabOpts = require('../utils/grab-opts');
 var debug = require('debug')('castnow:transcode');
 var port = 4103;
+var chromeSupportedVid = ['h264', 'vp8'];
+var chromeSupportedAud = ['aac', 'mp3', 'vorbis', 'pcm_s16le', 'flac'];
 
 function shouldTranscode(ctx) {
   return ctx.options.transcode || ctx.options.tomp4;
@@ -28,48 +30,54 @@ var transcode = function(ctx, next) {
     res.writeHead(200, {
       'Access-Control-Allow-Origin': '*'
     });
-    var s = got(orgPath);
-    s.on('error', function(err) {
-      debug('got error: %o', err);
-    });
 
-    var detect = new Transcoder(s)
+    var detect = new Transcoder(orgPath)
       .on('metadata', function(meta) {
         var videoCodec, audioCodec;
-        for (var strm in meta.input.streams) {
-          if (strm.type === "video") {
+        for (var i = 0; i < meta.input.streams.length; i++) {
+          var strm = meta.input.streams[i];
+          if (strm.type === 'video') {
             videoCodec = strm.codec;
-          } else if (strm.type === "audio") {
+          } else if (strm.type === 'audio') {
             audioCodec = strm.codec;
           }
         }
         debug('got input metadata. vid:%s, aud:%s', videoCodec, audioCodec);
+        //todo: a/v codecs ok?
+        //var s = got(orgPath);
+        //s.on('error', function(err) {
+        //  debug('got error: %o', err);
+        //});
+        doTranscode(orgPath, false, false, opts, res);
       })
       .exec();
-
-    var trans = new Transcoder(s)
-      .videoCodec('h264')
-      .format('matroska')
-      .custom('strict', 'experimental')
-      .on('finish', function() {
-        debug('finished transcoding');
-      })
-      .on('error', function(err) {
-        debug('transcoding error: %o', err);
-      });
-    for (var key in opts) {
-      trans.custom(key, opts[key]);
-    }
-
-    var args = trans._compileArguments();
-    args = [ '-i', '-' ].concat(args);
-    args.push('pipe:1');
-    debug('spawning ffmpeg %s', args.join(' '));
-
-    trans.stream().pipe(res);
   }).listen(port);
   debug('started webserver on address %s using port %s', ip, port);
   next();
 };
+
+var doTranscode = function(path, bAud, bVid, opts, res) {
+  var trans = new Transcoder(path)
+    .videoCodec(bVid === true ? 'copy' : 'h264')
+    .audioCodec(bAud === true ? 'copy' : 'vorbis')
+    .format('matroska')
+    .custom('strict', 'experimental')
+    .on('finish', function() {
+      debug('finished transcoding');
+    })
+    .on('error', function(err) {
+      debug('transcoding error: %o', err);
+    });
+  for (var key in opts) {
+    trans.custom(key, opts[key]);
+  }
+
+  var args = trans._compileArguments();
+  args = [ '-i', '-' ].concat(args);
+  args.push('pipe:1');
+  debug('spawning ffmpeg %s', args.join(' '));
+
+  trans.stream().pipe(res);
+}
 
 module.exports = transcode;
